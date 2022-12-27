a417f056267404d7da4b1967865d19d4bea92086
Tunde Adetunji <tundeadetunji2017@gmail.com>
2022-12-27 18:37:36 +0100

    Add project files.

---
+++ MarkdownMaker/Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.dropElement = New System.Windows.Forms.ComboBox()
+        Me.textMarkup = New System.Windows.Forms.TextBox()
+        Me.buttonFont = New System.Windows.Forms.Button()
+        Me.FontDialog = New System.Windows.Forms.FontDialog()
+        Me.buttonSave = New System.Windows.Forms.Button()
+        Me.dropLanguage = New System.Windows.Forms.ComboBox()
+        Me.buttonSendToClipboard = New System.Windows.Forms.Button()
+        Me.buttonOpen = New System.Windows.Forms.Button()
+        Me.buttonElement = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'dropElement
+        '
+        Me.dropElement.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dropElement.FormattingEnabled = True
+        Me.dropElement.Location = New System.Drawing.Point(390, 14)
+        Me.dropElement.Name = "dropElement"
+        Me.dropElement.Size = New System.Drawing.Size(341, 33)
+        Me.dropElement.TabIndex = 1
+        '
+        'textMarkup
+        '
+        Me.textMarkup.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.textMarkup.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.textMarkup.Location = New System.Drawing.Point(13, 53)
+        Me.textMarkup.Multiline = True
+        Me.textMarkup.Name = "textMarkup"
+        Me.textMarkup.Size = New System.Drawing.Size(934, 360)
+        Me.textMarkup.TabIndex = 0
+        '
+        'buttonFont
+        '
+        Me.buttonFont.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.buttonFont.Image = Global.MarkdownMaker.My.Resources.Resources.Apps_preferences_desktop_font_icon_24
+        Me.buttonFont.Location = New System.Drawing.Point(881, 14)
+        Me.buttonFont.Name = "buttonFont"
+        Me.buttonFont.Size = New System.Drawing.Size(66, 33)
+        Me.buttonFont.TabIndex = 3
+        Me.buttonFont.UseVisualStyleBackColor = True
+        '
+        'buttonSave
+        '
+        Me.buttonSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.buttonSave.Image = Global.MarkdownMaker.My.Resources.Resources._2_Hot_Printer_icon_24
+        Me.buttonSave.Location = New System.Drawing.Point(737, 14)
+        Me.buttonSave.Name = "buttonSave"
+        Me.buttonSave.Size = New System.Drawing.Size(66, 33)
+        Me.buttonSave.TabIndex = 2
+        Me.buttonSave.UseVisualStyleBackColor = True
+        '
+        'dropLanguage
+        '
+        Me.dropLanguage.FormattingEnabled = True
+        Me.dropLanguage.Location = New System.Drawing.Point(85, 14)
+        Me.dropLanguage.Name = "dropLanguage"
+        Me.dropLanguage.Size = New System.Drawing.Size(227, 33)
+        Me.dropLanguage.TabIndex = 4
+        '
+        'buttonSendToClipboard
+        '
+        Me.buttonSendToClipboard.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.buttonSendToClipboard.Image = Global.MarkdownMaker.My.Resources.Resources.Alarm_Arrow_Right_icon_24
+        Me.buttonSendToClipboard.Location = New System.Drawing.Point(809, 14)
+        Me.buttonSendToClipboard.Name = "buttonSendToClipboard"
+        Me.buttonSendToClipboard.Size = New System.Drawing.Size(66, 33)
+        Me.buttonSendToClipboard.TabIndex = 5
+        Me.buttonSendToClipboard.UseVisualStyleBackColor = True
+        '
+        'buttonOpen
+        '
+        Me.buttonOpen.Image = Global.MarkdownMaker.My.Resources.Resources.text_x_markdown_icon_24
+        Me.buttonOpen.Location = New System.Drawing.Point(13, 14)
+        Me.buttonOpen.Name = "buttonOpen"
+        Me.buttonOpen.Size = New System.Drawing.Size(66, 33)
+        Me.buttonOpen.TabIndex = 7
+        Me.buttonOpen.UseVisualStyleBackColor = True
+        '
+        'buttonElement
+        '
+        Me.buttonElement.Image = Global.MarkdownMaker.My.Resources.Resources.Alarm_Tick_icon_24
+        Me.buttonElement.Location = New System.Drawing.Point(318, 14)
+        Me.buttonElement.Name = "buttonElement"
+        Me.buttonElement.Size = New System.Drawing.Size(66, 33)
+        Me.buttonElement.TabIndex = 8
+        Me.buttonElement.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(961, 427)
+        Me.Controls.Add(Me.buttonElement)
+        Me.Controls.Add(Me.buttonOpen)
+        Me.Controls.Add(Me.buttonSendToClipboard)
+        Me.Controls.Add(Me.dropLanguage)
+        Me.Controls.Add(Me.buttonSave)
+        Me.Controls.Add(Me.buttonFont)
+        Me.Controls.Add(Me.textMarkup)
+        Me.Controls.Add(Me.dropElement)
+        Me.Font = New System.Drawing.Font("Cascadia Code ExtraLight", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(5)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Markdown Maker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dropElement As ComboBox
+    Friend WithEvents textMarkup As TextBox
+    Friend WithEvents buttonFont As Button
+    Friend WithEvents FontDialog As FontDialog
+    Friend WithEvents buttonSave As Button
+    Friend WithEvents dropLanguage As ComboBox
+    Friend WithEvents buttonSendToClipboard As Button
+    Friend WithEvents buttonOpen As Button
+    Friend WithEvents buttonElement As Button
+End Class
---
+++ MarkdownMaker/Form1.vb
@@ -0,0 +1,91 @@
+﻿Imports iNovation.Code.Desktop
+Imports iNovation.Code.General
+Imports MarkdownMaker.KB
+Public Class Form1
+    Private Sub buttonFont_Click(sender As Object, e As EventArgs) Handles buttonFont.Click
+        FontDialog.ShowDialog()
+        textMarkup.Font = FontDialog.Font
+
+    End Sub
+
+    Private Sub buttonSave_Click(sender As Object, e As EventArgs) Handles buttonSave.Click
+        SaveFile(Content(textMarkup), FileKind.Markdown, "Choose where to save your file", "ReadMe")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        setupUI()
+    End Sub
+
+    Private Sub setupUI()
+        BindProperty(dropLanguage, GetEnum(New defaultLanguage))
+        BindProperty(dropElement, GetEnum(New elements))
+        formatDrops({dropLanguage, dropElement})
+        textMarkup.Text = "### welcome"
+    End Sub
+
+    Sub formatDrops(drops As Array)
+        If drops.Length = 0 Then Return
+        Dim d As ComboBox
+        For i = 0 To drops.Length - 1
+            d = CType(drops(i), ComboBox)
+            d.AutoCompleteMode = AutoCompleteMode.Suggest
+            d.AutoCompleteSource = AutoCompleteSource.ListItems
+        Next
+    End Sub
+
+    Private Sub dropMarkup_SelectedIndexChanged(sender As Object, e As EventArgs) Handles dropElement.SelectedIndexChanged
+        Dim t As TextBox = textMarkup
+        Dim drop As ComboBox = dropElement
+
+        If t.SelectionLength < 1 Then
+            If Content(drop) = elements.i.ToString Or
+                    Content(drop) = elements.strong.ToString Or
+                    Content(drop) = elements.strikethrough.ToString Or
+                    Content(drop) = elements.a_verbose.ToString Or
+                    Content(drop) = elements.a.ToString Or
+                    Content(drop) = elements.img.ToString Or
+                    Content(drop) = elements.code.ToString Or
+                    Content(drop) = elements.pre.ToString Or
+                    Content(drop) = elements.video.ToString Then
+                Return
+            End If
+        End If
+
+
+        Dim pre As String = t.Text.Substring(0, t.SelectionStart)
+        Dim suf As String = t.Text.Substring(t.SelectionLength + t.SelectionStart)
+        Dim text As String = t.Text.Substring(t.SelectionStart, t.SelectionLength)
+
+        Dim code As List(Of String) = getSnippet(Content(dropElement), text, Content(dropLanguage))
+
+        textMarkup.Focus()
+        Dim s = t.SelectionStart
+        Dim l = t.SelectionLength
+
+        If dropElement.Text = elements.a.ToString Then
+            t.Text = pre & code(0) ''& text & code(1) & suf
+        ElseIf dropElement.Text = elements.img.ToString Then
+            t.Text = pre & code(0) ''& text & code(1) & suf
+        ElseIf dropElement.Text = elements.video.ToString Then
+            t.Text = pre & code(0) ''& text & code(1) & suf
+        Else
+            t.Text = pre & code(0) & text & code(1) & suf
+        End If
+
+        t.ScrollToCaret()
+
+    End Sub
+
+    Private Sub buttonSendToClipboard_Click(sender As Object, e As EventArgs) Handles buttonSendToClipboard.Click
+        Dim t As String = Content(textMarkup)
+        If t.Length > 0 Then Clipboard.SetText(t)
+    End Sub
+
+    Private Sub buttonOpen_Click(sender As Object, e As EventArgs) Handles buttonOpen.Click
+        GetFile(textMarkup, FileDialogExpectedFormat.code)
+    End Sub
+
+    Private Sub buttonElement_Click(sender As Object, e As EventArgs) Handles buttonElement.Click
+        dropMarkup_SelectedIndexChanged(sender, e)
+    End Sub
+End Class
